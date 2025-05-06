ca99076a9bc346757c4f6e81aabe0cfdc0fc6fd5
Karoliny Franco <karoliny.franco@gmail.com>
2025-05-06 16:58:58 -0300

    Ajustando posição do footer - Versão 1.3.2

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -4,20 +4,20 @@ import AppNavbar from "./Navbar";
 
 export default function MainLayout({ children }: PropsWithChildren) {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <AppNavbar />
-      <Container fluid className="d-flex justify-content-center">
+      <Container fluid className="flex-grow-1 d-flex justify-content-center">
         <Row style={{ width: "100%", maxWidth: "900px" }}>
           <Col>{children}</Col>
         </Row>
       </Container>
-      <footer className="bg-primary text-white text-center py-3 mt-auto">
+      <footer className="bg-primary text-white text-center py-3">
         <Container>
           <span style={{ display: "inline-block", minHeight: "24px" }}>
             © 2025 Sistema Banestes - Todos os direitos reservados.
           </span>
         </Container>
       </footer>
-    </>
+    </div>
   );
 }
